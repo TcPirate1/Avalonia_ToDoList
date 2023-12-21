8895705f6f881fe948a526b1cc05f19af142229b
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2023-12-21 14:25:15 +1300

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+using ToDoList.DataModel;
+using ToDoList.Services;
+
+namespace ToDoList.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        //this has a dependency on the ToDoListService
+
+        private ViewModelBase _contentViewModel;
+         public MainWindowViewModel()
+        {
+            var service = new ToDoListService();
+            ToDoList = new ToDoListViewModel(service.GetItems());
+            _contentViewModel = ToDoList;
+        }
+        
+        public ViewModelBase ContentViewModel
+        {
+            get => _contentViewModel;
+            private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+        }
+
+        public ToDoListViewModel ToDoList { get; }
+
+        public void AddItem()
+        {
+            AddItemViewModel addItemViewModel = new();
+
+            Observable.Merge(
+                addItemViewModel.OkCommand,
+                addItemViewModel.CancelCommand.Select(_ => (ToDoItem?)null))
+                .Take(1)
+                // At this point I encountered an error. If this happens in future look at this https://stackoverflow.com/questions/9454030/error-cannot-convert-lambda-expression-in-subscribe-for-an-iobservablepoint
+                // This as well https://developercommunity.visualstudio.com/t/quick-actions-and-refactorings-cs1660-cannot-conve/1222870
+                // TLDR: Subscribe method needs "Using System;"
+                .Subscribe(newItem =>
+                {
+                    if (newItem != null)
+                    {
+                        ToDoList.ListItems.Add(newItem );
+                    }
+                    ContentViewModel = ToDoList;
+                });
+
+            ContentViewModel = addItemViewModel;
+        }
+    }
+}
---
+++ ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.DataModel;
+
+namespace ToDoList.ViewModels
+{
+    public class ToDoListViewModel(IEnumerable<ToDoItem> items) : ViewModelBase
+    {
+        public ObservableCollection<ToDoItem> ListItems { get; } = new ObservableCollection<ToDoItem>(items);
+    }
+}
